603425537232fda5869fbc56fbe0efa5036d575c
Onda-dev <manuelaltieri3@gmail.com>
2022-04-28 14:35:21 +0200

    Aggiunto limite decimali

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ const etaPasseggero = Number(prompt('Quanti anni hai?'))
 // CALCOLO PREZZO BIGLIETTO (0.21€ al KM - SCONTI)
 let prezzoBiglietto;
 if (etaPasseggero < 18) {
-    prezzoBiglietto = (kmTragitto * 0.21) - (kmTragitto * 0.21 / 100 * 20);
+    prezzoBiglietto = ((kmTragitto * 0.21) - (kmTragitto * 0.21 / 100 * 20)).toFixed(2);
 } else if (etaPasseggero > 65) {
-    prezzoBiglietto = (kmTragitto * 0.21) - (kmTragitto * 0.21 / 100 * 40);
+    prezzoBiglietto = ((kmTragitto * 0.21) - (kmTragitto * 0.21 / 100 * 40)).toFixed(2);
 } else {
-    prezzoBiglietto = kmTragitto * 0.21;
+    prezzoBiglietto = (kmTragitto * 0.21).toFixed(2);
 }
 
 console.log(prezzoBiglietto)
